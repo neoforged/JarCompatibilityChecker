2775d65bd437329f4ebce4e49cd2b1f9ef02d3a6
mezz <tehgeek@gmail.com>
2026-09-13 16:56:53 +0900

    Fix #9 Redeclaration of abstract method in a subclass should be compatible (#10)

--- src/test/java/net/neoforged/jarcompatibilitychecker/test/InterfaceTests.java
+++ src/test/java/net/neoforged/jarcompatibilitychecker/test/InterfaceTests.java
@@ -5,10 +5,19 @@
 
 package net.neoforged.jarcompatibilitychecker.test;
 
+import com.google.common.collect.ImmutableMap;
+import net.neoforged.jarcompatibilitychecker.core.ClassInfoCache;
+import net.neoforged.jarcompatibilitychecker.core.ClassInfoComparer;
+import net.neoforged.jarcompatibilitychecker.core.ClassInfoComparisonResults;
 import net.neoforged.jarcompatibilitychecker.core.IncompatibilityMessages;
+import net.neoforged.jarcompatibilitychecker.data.ClassInfo;
 import org.junit.Test;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class InterfaceTests extends BaseCompatibilityTest {
     @Override
@@ -62,5 +71,48 @@ public class InterfaceTests extends BaseCompatibilityTest {
     public void testNewInterface() {
         // A new interface is binary compatible
         assertCompatible(true, "NewInterface", "A");
+    }
+
+    @Test
+    public void testRedeclaredInheritedAbstractMethod() {
+        ClassInfo baseParent = createInterface("A", Opcodes.ACC_PUBLIC | Opcodes.ACC_ABSTRACT);
+        ClassInfo baseChild = createInterface("B", 0, "A");
+
+        ClassInfo inputParent = createInterface("A", Opcodes.ACC_PUBLIC | Opcodes.ACC_ABSTRACT);
+        ClassInfo inputChild = createInterface("B", Opcodes.ACC_PUBLIC | Opcodes.ACC_ABSTRACT, "A");
+
+        ClassInfoCache baseCache = ClassInfoCache.fromMaps(ImmutableMap.of("A", baseParent, "B", baseChild), ImmutableMap.of());
+        ClassInfoCache inputCache = ClassInfoCache.fromMaps(ImmutableMap.of("A", inputParent, "B", inputChild), ImmutableMap.of());
+        ClassInfoComparisonResults results = ClassInfoComparer.compare(true, baseCache, baseChild, inputCache, inputChild);
+
+        assertCompatible(results, "B");
+    }
+
+    @Test
+    public void testInheritedDefaultMethodMadeAbstract() {
+        ClassInfo baseParent = createInterface("A", Opcodes.ACC_PUBLIC);
+        ClassInfo baseChild = createInterface("B", 0, "A");
+
+        ClassInfo inputParent = createInterface("A", Opcodes.ACC_PUBLIC);
+        ClassInfo inputChild = createInterface("B", Opcodes.ACC_PUBLIC | Opcodes.ACC_ABSTRACT, "A");
+
+        ClassInfoCache baseCache = ClassInfoCache.fromMaps(ImmutableMap.of("A", baseParent, "B", baseChild), ImmutableMap.of());
+        ClassInfoCache inputCache = ClassInfoCache.fromMaps(ImmutableMap.of("A", inputParent, "B", inputChild), ImmutableMap.of());
+        ClassInfoComparisonResults results = ClassInfoComparer.compare(true, baseCache, baseChild, inputCache, inputChild);
+
+        assertIncompatible(results, "B", "abstractMethod", "()V", true, IncompatibilityMessages.METHOD_MADE_ABSTRACT);
+    }
+
+    private static ClassInfo createInterface(String name, int methodAccess, String... interfaces) {
+        ClassNode node = new ClassNode();
+        node.version = Opcodes.V1_8;
+        node.access = Opcodes.ACC_PUBLIC | Opcodes.ACC_INTERFACE | Opcodes.ACC_ABSTRACT;
+        node.name = name;
+        node.superName = "java/lang/Object";
+        node.interfaces.addAll(Arrays.asList(interfaces));
+        if (methodAccess != 0) {
+            node.methods.add(new MethodNode(methodAccess, "abstractMethod", "()V", null, null));
+        }
+        return new ClassInfo(node);
     }
 }
